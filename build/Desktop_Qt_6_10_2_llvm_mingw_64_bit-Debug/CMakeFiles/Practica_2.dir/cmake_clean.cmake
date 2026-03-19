d87bb5d01d5da9ecf9c55ebfea8cab1aa62e2d5b
JhonCastro18 <jhon.castro1@udea.edu.co>
2026-03-19 13:13:53 -0500

    Primer commit practica 2 problemas impares del 1 al 9

---
+++ build/Desktop_Qt_6_10_2_llvm_mingw_64_bit-Debug/CMakeFiles/Practica_2.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Practica_2.dir/main.cpp.obj"
+  "CMakeFiles/Practica_2.dir/main.cpp.obj.d"
+  "Practica_2.exe"
+  "Practica_2.exe.manifest"
+  "Practica_2.pdb"
+  "libPractica_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Practica_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/Desktop_Qt_6_10_2_llvm_mingw_64_bit-Debug/CMakeFiles/Practica_2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Practica_2.
+# This may be replaced when dependencies are built.
